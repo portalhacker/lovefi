f8a8e9707331784740908a912df0f6ba7f70971e
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2025-07-11 23:58:50 -0400

    chore: fastapi initial tutorial with base model

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -1,9 +1,15 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
@@ -14,3 +20,9 @@ async def read_root() -> dict[str, str]:
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, Union[int, str, None]]:
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
